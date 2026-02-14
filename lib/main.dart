e5260cd2210fcc5ae605bd2d5bebb65d74c46d74
Chamuditha Pasindu <chamudithapasindu54@gmail.com>
2026-02-14 08:21:15 +0530

    image gen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:naleemiah_erp/utils/splash_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const NaleemiahErpApp());
 }
 
