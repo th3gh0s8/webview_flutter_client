4957421bdf13405e2f269bf08bc725af4f432587
Chamuditha Pasindu <chamudithapasindu54@gmail.com>
2026-05-19 10:45:00 +0530

    updates

--- lib/utils/splash_screen.dart
+++ lib/utils/splash_screen.dart
@@ -57,8 +57,8 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
               // Local asset logo
               Image.asset(
                 'assets/images/ncc_logo_1_485_46.png',
-                width: 200,
-                height: 200,
+                width: 300,
+                fit: BoxFit.contain,
               ),
             ],
           ),
